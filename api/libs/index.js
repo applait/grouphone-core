f6f9ff3cc5aed0b867a22116b3d7d90790e8c266
Soumya Deb <debloper@gmail.com>
2015-04-04 20:01:13 +0530

    Login API implemented

--- api/libs/index.js
+++ api/libs/index.js
@@ -2,6 +2,33 @@
  * Shared methods for API calls to reuse
  */
 
-var libs = {}
+var libs = {
+  findUser: function (email) {
+    db.accounts.findOne({ email: email }, function (err, doc) {
+      if (err) return err;
+
+      if (doc && doc.email) {
+          return doc;
+      } else return null;
+    });
+  },
+
+  activateUser: function (email, callback) {
+    db.accounts.update(
+      { email: email },
+      {
+        $set: { isActive: true },
+        $currentDate: { lastModified: true }
+      },
+      function (err) {
+        if (err) return callback(err);
+        db.activations.remove( { email: email }, function (error) {
+          // @TODO: maybe rollback isActive?
+          if (error)  return callback(error);
+        });
+      }
+    );
+  }
+}
 
 module.exports = libs;
--- api/login.js
+++ api/login.js
@@ -4,21 +4,52 @@
 
 var router = require("express").Router();
 
+// API to handle login requests from clients
+// Expects username & hashed password in request body
 router.post("/", function (req, res) {
-  /*
-    1. API server receives a request from Client
-    2. API expects username & hashed password
-    3. API looks up on database for the user
-      i. If the user is found for the email
-        a. And password matched
-          - Check if "isActive" is false
-            # Set "isActive" true in accounts
-            # Remove password reset token
-          - Send success with user-datails
-        b. And password didn't match, send error
-      ii. If the user isn't found, send error
-  */
-  res.status(200).json({});
+
+  // API looks up on database for the user
+  var user = libs.findUser(req.body.email);
+
+  // If the user is found for the email
+  if (user) {
+
+    // And password matched
+    if ( user.password == req.body.password) {
+
+      // Check if "isActive" is false
+      if (!user.isActive) {
+        // Set "isActive" true in accounts
+        // Remove password reset token in activations
+        libs.activateUser(user.email, function (error) {
+          res.status(500).json({
+            error: error,
+            message: "DB operation failed"
+          });
+        });
+      }
+
+      // Strip off password data from user-info
+      delete user.password;
+
+      // Send success with user-datails
+      res.status(200).json(user);
+    } else {
+
+      // So, the password didn't match
+      res.status(401).json({
+        error: 401,
+        message: "Password didn't match."
+      });
+    }
+  } else {
+
+    // Alright, the user wasn't found
+    res.status(404).json({
+      error: 404,
+      message: "Couldn't find the user."
+    });
+  }
 });
 
 module.exports = router;
