6d74c35e71a51970e5647e1a28cfb9d83352a70c
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-05 23:25:56 +0530

    Add 2 spaces emacs indentation for js2-mode and web-mode
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((js2-mode
+  (js-indent-level . 2)
+  (js2-indent-level . 2)
+  (js2-basic-offset . 2))
+ (web-mode
+  (web-mode-code-indent-offset . 2)
+  (web-mode-css-indent-offset . 2)
+  (web-mode-markup-indent-offset . 2)))
