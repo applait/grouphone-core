7e1e121f810f2bc4990d456a8201b2823dd610c3
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-05 23:33:55 +0530

    Update /api/passwd to new callbac signature
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- api/passwd.js
+++ api/passwd.js
@@ -9,17 +9,18 @@ var router = require("express").Router();
 router.get("/:token", function (req, res) {
 
   // API looks up on activations for existing request
-  libs.validateToken(req.params.token, function (result) {
+  libs.validateToken(req.params.token, function (err, result) {
+
+    if (err || !result) {
+      // Send back error if the record wasn't found
+      return res.status(500).json({
+        error: error,
+        message: "No pending password reset request for the user."
+      });
+    }
 
     // Send back email along with the token
     return res.status(200).json(result);
-  }, function (error) {
-
-    // Send back error if the record wasn't found
-    return res.status(500).json({
-      error: error,
-      message: "No pending password reset request for the user."
-    });
   });
 });
 
@@ -28,27 +29,29 @@ router.get("/:token", function (req, res) {
 router.post("/", function (req, res) {
 
   // API looks up on activations for existing request
-  libs.validateToken(req.body.token, function (result) {
+  libs.validateToken(req.body.token, function (err, result) {
+
+    if (err || !result) {
+      // Send back error if the record wasn't found
+      return res.status(500).json({
+        error: error,
+        message: "No pending password reset request for the user."
+      });
+    }
 
     // Update password on accounts collection
-    libs.updatePassword(req.body, function () {
+    libs.updatePassword(req.body, function (err) {
+
+      if (err) {
+        // Return error is update password fails
+        return res.status(500).json({
+          error: error,
+          message: "DB operation failed"
+        });
+      }
 
       // Send success silently
       return res.status(200).json({ success: true });
-    }, function (error) {
-
-      // Return error is update password fails
-      return res.status(500).json({
-        error: error,
-        message: "DB operation failed"
-      });
-    });
-  }, function (error) {
-
-    // Send back error if the record wasn't found
-    return res.status(500).json({
-      error: error,
-      message: "No pending password reset request for the user."
     });
   });
 });
