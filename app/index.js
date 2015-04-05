7f30333a8857a11df7dbef24c1db84e8e47d42ab
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-05 19:43:37 +0530

    First pass at querying /api/login from client's POST /login
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- app/index.js
+++ app/index.js
@@ -3,7 +3,9 @@
  * Returns views for every request
  */
 
-var router = require("express").Router();
+var router = require("express").Router(),
+    request = require("request"),
+    apibase = "http://" + config.API_HOSTNAME + ":" + config.API_PORT;
 
 // The landing page of the website
 router.get("/", function (req, res) {
@@ -23,7 +25,23 @@ router.get("/login", function (req, res) {
 // For a post request, process the given payload
 // Send user the /app page on successful login
 router.post("/login", function (req, res) {
-    res.status(200).redirect("/app");
+  // Query the login API
+  request.post(
+    { url: apibase + "/api/login",
+      form: { email: req.body.email, password: req.body.password }},
+    function (err, response, body) {
+      if (!err && response.statusCode == 200) {
+        // User credentials matched. Create session. Redirect to app landing page.
+        console.log(JSON.parse(body));
+        res.redirect("/app");
+      } else {
+        // User did not match. Redirect back to login page with error message.
+        console.log("Response status: ", response.statusCode);
+        console.log("Response body: ", JSON.parse(body));
+        if (err) console.log("Error", err);
+        res.redirect("/login?failed=1");
+      }
+    });
 });
 
 module.exports = router;
