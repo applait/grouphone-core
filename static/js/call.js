36bef99f5d3feb81e4c9ebc7787c33c4c410da3b
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-08 02:22:00 +0530

    Refactor call.js to make room for room.connect()

--- static/js/call.js
+++ static/js/call.js
@@ -33,16 +33,6 @@ var initcall = function (sessionid, username) {
         }
     };
 
-    var subscribeall = function (streamslist) {
-        for (var index in streamslist) {
-            console.log("Subscribing Streams", streamslist[index]);
-            var currentstream = streamslist[index];
-            if (localstream.getID() !== currentstream.getID()) {
-                room.subscribe(currentstream);
-            }
-        }
-    };
-
     var createcall = function () {
         var callid = sessionid && sessionid.trim(),
             callmethod = "call:create",
@@ -69,61 +59,62 @@ var initcall = function (sessionid, username) {
             localstream = Erizo.Stream({ audio: true, video: false, data: false });
             room = Erizo.Room({ roomID: data.session.data.room._id, token: token});
 
-            room.addEventListener("room-connected", function (roomevent) {
-                console.log("Connected to room");
-                subscribeall(roomevent.streams);
-                var checkandpublish = function () {
-                    setTimeout(function () {
-                        if (localstream.stream) {
-                            console.log("localstream is ready");
-                            localstream.play("localaudio");
-                            room.publish(localstream, { maxAudioBW: 24}, function (pubid, err) {
-                                if (pubid === undefined) {
-                                    console.log("Error publishing stream", err);
-                                } else {
-                                    console.log("Published stream", pubid);
-                                }
-                            });
-                        } else {
-                            console.log("localstream is not ready yet");
-                            checkandpublish();
-                        }
-                    }, 100);
-                };
-                checkandpublish();
-            });
-
-            room.addEventListener("stream-subscribed", function (streamevent) {
-                console.log("Stream subscribed");
-                var div = document.createElement("div");
-                div.setAttribute("id", "stream-" + streamevent.stream.getID());
-                document.querySelector("#audiocontainer").appendChild(div);
-                streamevent.stream.play("stream-" + streamevent.stream.getID());
-            });
-
-            room.addEventListener("stream-added", function (streamevent) {
-                console.log("Stream added fired");
-                var streams = [];
-                streams.push(streamevent.stream);
-                subscribeall(streams);
-            });
-
-            room.addEventListener("stream-failed", function (){
-                console.log("STREAM FAILED, DISCONNECTION");
-                room.disconnect();
-            });
-
-            room.addEventListener("stream-removed", function (streamevent) {
-                if (streamevent.stream.elementID !== undefined) {
-                    var streamer = document.getElementById(streamevent.stream.elementID);
-                    if (streamer) {
-                        document.querySelector("#audiocontainer").removeChild(streamer);
-                    }
-                }
-            });
-
             localstream.addEventListener("access-accepted", function () {
+
+                var subscribeall = function (streamslist) {
+                    for (var index in streamslist) {
+                        var currentstream = streamslist[index];
+                        if (localstream.getID() !== currentstream.getID()) {
+                            console.log("Subscribing Stream", currentstream);
+                            room.subscribe(currentstream);
+                        }
+                    }
+                };
+
+                room.addEventListener("room-connected", function (roomevent) {
+                    console.log("Connected to room");
+                    room.publish(localstream, { maxAudioBW: 24}, function (pubid, err) {
+                        if (pubid === undefined) {
+                            console.log("Error publishing stream", err);
+                        } else {
+                            console.log("Published stream", pubid);
+                        }
+                    });
+                    subscribeall(roomevent.streams);
+                });
+
+                room.addEventListener("stream-subscribed", function (streamevent) {
+                    console.log("Stream subscribed. Trying to play", streamevent.stream);
+                    var div = document.createElement("div");
+                    div.setAttribute("id", "stream-" + streamevent.stream.getID());
+                    document.querySelector("#audiocontainer").appendChild(div);
+                    streamevent.stream.play("stream-" + streamevent.stream.getID());
+                });
+
+                room.addEventListener("stream-added", function (streamevent) {
+                    console.log("Stream added fired");
+                    var streams = [];
+                    streams.push(streamevent.stream);
+                    subscribeall(streams);
+                });
+
+                room.addEventListener("stream-failed", function (){
+                    console.log("STREAM FAILED, DISCONNECTION");
+                    room.disconnect();
+                });
+
+                room.addEventListener("stream-removed", function (streamevent) {
+                    console.log("Removing stream", streamevent.stream);
+                    if (streamevent.stream.elementID !== undefined) {
+                        var streamer = document.getElementById(streamevent.stream.elementID);
+                        if (streamer) {
+                            document.querySelector("#audiocontainer").removeChild(streamer);
+                        }
+                    }
+                });
+
                 room.connect();
+                localstream.play("localaudio");
             });
 
             localstream.init();
