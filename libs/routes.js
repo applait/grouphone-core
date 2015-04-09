352fd0937bdc48a194940e6817ac44ea406f7a8c
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-09 23:48:11 +0530

    Implement forgot password and reset password.
    Fixes #2, Fixes #5 and Fixes #21
    
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- libs/routes.js
+++ libs/routes.js
@@ -8,6 +8,10 @@ var router = require("express").Router(),
     apibase = "http://" + config.API_HOSTNAME + ":" + config.API_PORT,
     crypto = require("crypto");
 
+var hash = function (string) {
+  return crypto.createHmac("sha1", config.SALT).update(string).digest("hex");
+};
+
 // The landing page of the website
 router.get("/", function (req, res) {
   res.status(200).sendFile(approot + "static/index.html");
@@ -26,10 +30,18 @@ router.get("/login", noauth, function (req, res) {
 // For a post request, process the given payload
 // Send user the /app page on successful login
 router.post("/login", noauth, function (req, res) {
+  var email = req.body && req.body.email && req.body.email.trim();
+  var password = req.body && req.body.password && req.body.password.trim();
+
+  if (!email || !password) {
+    return res.redirect("/login?failed=1");
+  }
+  password = hash(password);
+
   // Query the login API
   request.post(
     { url: apibase + "/api/login",
-      form: { email: req.body.email, password: req.body.password }},
+      form: { email: email, password: password }},
     function (err, response, body) {
       if (!err && response.statusCode == 200) {
         // User credentials matched. Create session. Redirect to app landing page.
@@ -73,6 +85,80 @@ router.get("/call", auth, function (req, res) {
 router.get("/join/:sessionid", function (req, res) {
   var email = crypto.randomBytes(2).toString("hex") + "@guest.grouphone.me";
   res.render("call", { sessionid: req.params.sessionid, user: { email: email } });
+});
+
+router.get("/forgot", noauth, function (req, res) {
+  res.render("forgot");
+});
+
+router.post("/forgot", noauth, function (req, res) {
+  var email = req.body && req.body.email && req.body.email.trim();
+
+  if (!email) {
+    return res.redirect("/login");
+  }
+
+  // Query the forgot API
+  request.post(
+    { url: apibase + "/api/forgot",
+      form: { email: email }},
+    function (err, response, body) {
+      if (!err && response.statusCode == 200) {
+        // User credentials matched. Create session. Redirect to app landing page.
+        body = JSON.parse(body);
+        console.log("Forgot password request", body);
+        res.redirect("/login?forgot=1");
+      } else {
+        // User did not match. Redirect back to login page with error message.
+        if (err) console.log("Error", err);
+        res.redirect("/forgot?failed=1");
+      }
+    });
+});
+
+router.get("/forgot/:token", noauth, function (req, res) {
+
+  // Query to see if token is valid API
+  request.get(
+    { url: apibase + "/api/passwd/" + req.params.token },
+    function (err, response, body) {
+      if (!err && response.statusCode == 200) {
+        // Token matched show activate view
+        body = JSON.parse(body);
+        res.render("resetpassword", { user: body });
+      } else {
+        // Token did not match. Redirect back to login page with error message.
+        if (err) console.log("Error", err);
+        res.redirect("/login?activate=failed");
+      }
+    });
+});
+
+router.post("/forgot/:token", noauth, function (req, res) {
+
+  var email = req.body && req.body.email && req.body.email.trim();
+  var token = req.body && req.body.token && req.body.token.trim();
+  var password = req.body && req.body.password && req.body.password.trim();
+
+  if (!email || !token || !password) {
+    return res.redirect("/login");
+  }
+
+  // Query to see if token is valid API
+  request.post(
+    { url: apibase + "/api/passwd/",
+      form: { email: email, token: token, password: hash(password) }},
+    function (err, response, body) {
+      if (!err && response.statusCode == 200) {
+        // Token matched show activate view
+        body = JSON.parse(body);
+        res.redirect("/login?reset=1");
+      } else {
+        // Token did not match. Redirect back to login page with error message.
+        if (err) console.log("Error", err);
+        res.redirect("/login?reset=failed");
+      }
+    });
 });
 
 module.exports = router;
