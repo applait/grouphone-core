bb27ff8bc8a47eb491cabe9e42b9865f1301cc7a
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-11 17:25:03 +0530

    First pass at integrating new call UI with end button support
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- static/js/call.js
+++ static/js/call.js
@@ -1,123 +1,158 @@
-var localstream = null;
-var token = null;
-var room = null;
+window.addEventListener("DOMContentLoaded", function () {
+  var localstream = null;
+  var token = null;
+  var room = null;
+  var socket = null;
 
-var initcall = function (sessionid, username) {
+  var initcall = function () {
 
-  var socket = io.connect("http://conqueror.applait.com:8000/"),
-      callidholder = document.getElementById("callid");
+    var callinfo = $("#callInfo"),
+        calllink = $("#callLink input[type='text']");
 
-  socket.on("message", function (message) {
-    if (message.type) {
-      switch (message.type) {
-      case "CONNECTION":
-        checkcall();
-        break;
-      default:
-        console.log(message.value);
-        break;
+    socket = io.connect("http://conqueror.applait.com:8000/");
+
+    socket.on("message", function (message) {
+      if (message.type) {
+        switch (message.type) {
+        case "CONNECTION":
+          checkcall();
+          break;
+        case "STATUS":
+          callinfo.innerHTML = message.value;
+          break;
+        }
       }
-    }
-  });
+    });
 
-  var checkcall = function () {
-    if (!localstream && !token && !room) {
-      createcall();
-    } else {
-      console.log("Reconnecting call");
-      if (room && room.connect) {
-        room.disconnect();
-        alert("Connection dropped.");
+    var checkcall = function () {
+      if (!localstream && !token && !room) {
+        createcall();
+      } else {
+        // Try reconnecting here
+        callinfo.innerHTML = "Reconnecting...";
+      }
+    };
+
+    var getcreator = function (members) {
+      var creator = null;
+      var key;
+      for (key in members) {
+        if (members.hasOwnProperty(key) && members[key].creator) {
+          creator = members[key];
+        }
+      }
+      return creator;
+    };
+
+    var createcall = function () {
+      var callid = sessionid && sessionid.trim(),
+          callmethod = "call:create",
+          calldata = {};
+
+      if (callid) {
+        callmethod = "call:connect";
+        calldata.sessionid = callid;
       }
 
-    }
-  };
+      calldata.username = username;
 
-  var createcall = function () {
-    var callid = sessionid && sessionid.trim(),
-        callmethod = "call:create",
-        calldata = {};
+      socket.emit(callmethod, calldata, function (err, data) {
+        if (err) {
+          console.log(err);
+          callinfo.innerHTML = "Unable to create call";
+          return;
+        }
 
-    console.log(callid);
-
-    if (callid) {
-      callmethod = "call:connect";
-      calldata.sessionid = callid;
-    }
-
-    calldata.username = username;
-
-    socket.emit(callmethod, calldata, function (err, data) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-
-      token = data.token;
-      callidholder.innerHTML = data.session.id;
-
-      localstream = Erizo.Stream({ audio: true, video: false, data: false });
-      room = Erizo.Room({ roomID: data.session.data.room._id, token: token});
-
-      localstream.addEventListener("access-accepted", function () {
-
-        var subscribeall = function (streamslist) {
-          for (var index in streamslist) {
-            var currentstream = streamslist[index];
-            if (localstream.getID() !== currentstream.getID()) {
-              console.log("Subscribing Stream", currentstream);
-              room.subscribe(currentstream);
-            }
-          }
+        window.onbeforeunload = function () {
+          return "Call in progress. Navigating away will end call. You can always press the 'End' button.";
         };
 
-        room.addEventListener("room-connected", function (roomevent) {
-          console.log("Connected to room");
-          room.publish(localstream, { maxAudioBW: 24}, function (pubid, err) {
-            if (pubid === undefined) {
-              console.log("Error publishing stream", err);
-            } else {
-              console.log("Published stream", pubid);
+        var creator = getcreator(data.session.data.members);
+        var membercount = Object.keys(data.session.data.members).length;
+
+        token = data.token;
+
+        localstream = Erizo.Stream({ audio: true, video: false, data: false });
+        room = Erizo.Room({ roomID: data.session.data.room._id, token: token});
+
+        localstream.addEventListener("access-accepted", function () {
+
+          callinfo.innerHTML = "Got stream. Connecting to call...";
+
+          var subscribeall = function (streamslist) {
+            for (var index in streamslist) {
+              var currentstream = streamslist[index];
+              if (localstream.getID() !== currentstream.getID()) {
+                room.subscribe(currentstream);
+              }
+            }
+          };
+
+          room.addEventListener("room-connected", function (roomevent) {
+            room.publish(localstream, { maxAudioBW: 24}, function (pubid, err) {
+              if (pubid === undefined) {
+                console.log("Error publishing stream");
+                callinfo.innerHTML = "Unable to publish stream";
+              }
+            });
+            calllink.value = "http://grouphone.me/join/" + data.session.id;
+            callinfo.innerHTML = "Call started by " + creator.name + "<br>";
+            if ((membercount - 2) > 0) {
+              callinfo.innerHTML += "with " + (membercount - 2);
+              callinfo.innerHTML += (membercount - 2) > 1 ? " others" : " other";
+            }
+            subscribeall(roomevent.streams);
+          });
+
+          room.addEventListener("stream-subscribed", function (streamevent) {
+            var div = document.createElement("div");
+            div.setAttribute("id", "stream-" + streamevent.stream.getID());
+            document.querySelector("#audiocontainer").appendChild(div);
+            streamevent.stream.play("stream-" + streamevent.stream.getID());
+          });
+
+          room.addEventListener("stream-added", function (streamevent) {
+            var streams = [];
+            streams.push(streamevent.stream);
+            subscribeall(streams);
+          });
+
+          room.addEventListener("stream-failed", function (){
+            callinfo.innerHTML = "Stream dropped";
+            room.disconnect();
+          });
+
+          room.addEventListener("stream-removed", function (streamevent) {
+            if (streamevent.stream.elementID !== undefined) {
+              var streamer = document.getElementById(streamevent.stream.elementID);
+              if (streamer) {
+                document.querySelector("#audiocontainer").removeChild(streamer);
+              }
             }
           });
-          subscribeall(roomevent.streams);
+
+          room.connect();
+          localstream.play("localaudio");
         });
 
-        room.addEventListener("stream-subscribed", function (streamevent) {
-          console.log("Stream subscribed. Trying to play", streamevent.stream);
-          var div = document.createElement("div");
-          div.setAttribute("id", "stream-" + streamevent.stream.getID());
-          document.querySelector("#audiocontainer").appendChild(div);
-          streamevent.stream.play("stream-" + streamevent.stream.getID());
-        });
-
-        room.addEventListener("stream-added", function (streamevent) {
-          console.log("Stream added fired");
-          var streams = [];
-          streams.push(streamevent.stream);
-          subscribeall(streams);
-        });
-
-        room.addEventListener("stream-failed", function (){
-          console.log("STREAM FAILED, DISCONNECTION");
-          room.disconnect();
-        });
-
-        room.addEventListener("stream-removed", function (streamevent) {
-          console.log("Removing stream", streamevent.stream);
-          if (streamevent.stream.elementID !== undefined) {
-            var streamer = document.getElementById(streamevent.stream.elementID);
-            if (streamer) {
-              document.querySelector("#audiocontainer").removeChild(streamer);
-            }
-          }
-        });
-
-        room.connect();
-        localstream.play("localaudio");
+        localstream.init();
+        callinfo.innerHTML = "Waiting for you to share mic...";
       });
-
-      localstream.init();
-    });
+    };
   };
-};
+
+  // Start call
+  initcall(sessionid);
+  window.onunload = function () {
+    if (room) room.disconnect();
+    if (socket) socket.disconnect();
+  };
+
+  $("#endCall").addEventListener("click", function () {
+    if (room) room.disconnect();
+    if (socket) socket.disconnect();
+    window.onbeforeunload = null;
+    location.assign("/app");
+  });
+
+});
