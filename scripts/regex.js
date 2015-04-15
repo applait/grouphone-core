77e4aa2684e18bfb7ae99a814cffe7147c2489f2
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-15 16:09:50 +0530

    Made grouphone.js work with stupid messages

--- scripts/regex.js
+++ scripts/regex.js
@@ -3,8 +3,8 @@
 var fs = require("fs");
 
 var flux = fs.readFileSync(__dirname + "/../static/js/vendor/erizo.js").toString();
-flux = flux .replace(/L.Logger=[\s\S]+\(L\);/g, "")
-            .replace(/:L.Logger.*?[(\)\.)\r\n]?.*?\),?;?/g, ":function(){}")
-            .replace(/L.Logger.*?[\r\n]?.*?\),?;?/g, "");
+var fun = "function(){}";
+flux = flux.replace(/L.Logger=[\s\S]+\(L\);L=L/, ["L.Logger={info:", fun, ",debug:", fun, ",error:", fun, "};L=L"].join(""))
+           .replace(/console.log\([\w\W]+?\)/g, "console.log('You are looking under the hood!')");
 
 fs.writeFileSync(__dirname + "/../tmp/erizo.js", flux);
