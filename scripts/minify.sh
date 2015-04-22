4b24db71d75215233fcc37a55c60415161ae9d36
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-22 23:57:45 +0530

    Combine js utils into js/grouphone-utils.js
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- scripts/minify.sh
+++ scripts/minify.sh
@@ -8,5 +8,13 @@ node ./scripts/regex.js
 # Concatenate the js & minify them
 ./node_modules/uglifyjs/bin/uglifyjs tmp/erizo.js static/js/call.js -o static/js/grouphone.js
 
+# Concatenate utils scripts
+./node_modules/uglifyjs/bin/uglifyjs \
+    static/js/vendor/core-min.js \
+    static/js/vendor/enc-base64-min.js \
+    static/js/vendor/hmac-sha1.js \
+    static/js/utils.js \
+    -o static/js/grouphone-utils.js
+
 # Clean yo' shit
 rm -rf tmp
