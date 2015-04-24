ba398de2d3872747d2acdbcc37b91f612649460f
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2015-04-24 23:11:46 +0530

    Move auth.js with hash and base64decode to generic utils.js
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- libs/utils.js
+++ libs/utils.js
@@ -1,11 +1,11 @@
 var request = require("request"),
     csrflib = require("csrf")(),
-    apibase = "http://" + config.API_HOSTNAME + ":" + config.API_PORT;
+    crypto = require("crypto");
 
 module.exports = {
   auth: function (req, res, next) {
     if (req.user && req.user.email && req.user.token) {
-      request(apibase + "/api/verify/" + req.user.email + "/" + req.user.token, function (err, resp, body) {
+      request(config.API_PATH + "/api/verify/" + req.user.email + "/" + req.user.token, function (err, resp, body) {
         if (err) {
           console.log("error", err);
           return res.redirect("/logout");
@@ -48,5 +48,14 @@ module.exports = {
 
   csrfToken: function () {
     return csrflib.create(config.SALT);
+  },
+
+  hash: function (string) {
+    return crypto.createHmac("sha1", config.SALT).update(string).digest("hex");
+  },
+
+  base64decode: function (string) {
+    return new Buffer(decodeURIComponent(string), "base64").toString("hex");
   }
+
 };
